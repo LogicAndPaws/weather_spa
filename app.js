7023a1717b1fa0873e9e13779cda9ef3d4b1cbf0
LogicAndPaws <logicandpaws@gmail.com>
2020-05-05 10:56:49 +0300

    Initial Commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var NodeCouchDb = require('node-couchdb');
+var express = require('express');
+
+var spaApp = express();
+
+const APP_PORT = 3000;
+
+let TABLES = new Map([
+  ["USERS", "weather_users"],
+  ["ADMINS", "weather_admins"],
+  ["DATA", "weather_data"]
+])
+
+
+const db = new NodeCouchDb({
+  host: 'localhost',
+  protocol: 'http',
+  port: 5984,
+  auth: {
+    user: 'SunShade',
+    pass: '4141'
+  }
+});
+
+spaApp.use('/res', express.static(__dirname + '/res'));
+
+spaApp.get('/', function (req, res) {
+  res.sendFile(__dirname + '/static/main.html');
+});
+
+spaApp.listen(APP_PORT, function () {
+  var tablePromise = new Promise(initTables);
+  Promise.all([tablePromise]).then(values => {
+    console.log('INFO: App started on ' + APP_PORT);
+  })
+});
+
+function initTables(resolve, reject){
+  let initBase = db.listDatabases().then(
+    dbs => {
+      for (let tableName of TABLES.values()) {
+        let index = dbs.find( (value, index, array) => {
+          if (value === tableName) return true;
+          else return false;
+        })
+        if (index === undefined) {
+          db.createDatabase(tableName).then(
+            () => {
+              console.log("INFO: Table " + tableName + " created")
+            },
+            err => {
+              console.log("ERROR: " + err);
+            }
+          )
+        }
+      }
+  }, err => {
+      console.log("ERROR: " + err);
+  });
+  Promise.all([initBase]).then(values => {
+    console.log("INFO: Database is ready");
+    resolve(1)
+  });
+}
